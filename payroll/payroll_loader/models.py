9fe4bcdc9ac32cffbd8fde69f8aae2ee5ca909b9
tomwross <sonofinterflux@gmail.com>
2018-04-23 00:21:54 -0400

    Added docstrings to Django models

--- payroll/payroll_loader/models.py
+++ payroll/payroll_loader/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Report(models.Model):
+    """
+    Report footer ID and date uploaded
+    """
+
     # Report ID from report footer
     report_footer = models.IntegerField()
     # Date report loaded
@@ -15,6 +19,10 @@ class Report(models.Model):
 
 
 class JobGroup(models.Model):
+    """
+    Job group type (e.g. A or B) and hourly pay rate
+    """
+
     # Job group type (e.g. A, B)
     type = models.CharField(max_length=128)
     # Pay rate of job group
@@ -28,6 +36,10 @@ class JobGroup(models.Model):
 
 
 class Employee(models.Model):
+    """
+    Employee number from uploaded payroll file
+    """
+
     # Employee ID number from payroll file
     employee_number = models.CharField(primary_key=True, max_length=128)
 
@@ -39,8 +51,9 @@ class Employee(models.Model):
 
 class EmployeeJobGroup(models.Model):
     '''
-    Job group employee belongs to
+    Employees are members of job groups
     '''
+
     # Employee of this job group
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
     job_group = models.ForeignKey(JobGroup, on_delete=models.CASCADE)
@@ -53,6 +66,10 @@ class EmployeeJobGroup(models.Model):
 
 
 class PayCheque(models.Model):
+    """
+    Pay cheque calculated for pay period from uploaded payroll file
+    """
+
     pay_amount = models.DecimalField(max_digits=10, decimal_places=2)
     # Inclusive
     start_date = models.DateField()
@@ -70,6 +87,10 @@ class PayCheque(models.Model):
 
 
 class WorkDay(models.Model):
+    """
+    Hours worked on date for employee and job group relation
+    """
+    
     employee_job_group = models.ForeignKey(EmployeeJobGroup, on_delete=models.CASCADE)
     date = models.DateField()
     hours = models.DecimalField(max_digits=5, decimal_places=2)
