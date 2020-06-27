595cf1615566c0928b5549f80179b419ef842a69
Luiz Saggioro <luizfzsaggioro@gmail.com>
2020-06-27 11:16:08 -0400

    Add test for data returned from payroll service

--- test/api/test_payroll_api.py
+++ test/api/test_payroll_api.py
@@ -26,3 +26,27 @@ class TestPayrollApi(unittest.TestCase):
             self.assertTrue(len(result.json["payrollReport"]) == 1, result.json["payrollReport"])
             self.assertTrue("employeeReports" in result.json["payrollReport"])
             self.assertTrue(len(result.json["payrollReport"]["employeeReports"]) == 0, result.json["payrollReport"])
+
+    @patch.object(PayrollService, 'get_report')
+    def test_return_emp_1_info_when_info_is_present(self, mock_get_report):
+        mock_return = {
+            "employeeId": 1,
+            "payPeriod": {
+                "startDate": "2020-01-01",
+                "endDate": "2020-01-15"
+            },
+            "amountPaid": "$300.00"
+        }
+        mock_get_report.return_value = [mock_return]
+
+        with self.app.app_context():
+            result = self.payroll_api.get()
+
+            self.assertIsNotNone(result)
+            self.assertEqual(result.status_code, 200)
+            self.assertTrue(result.json)
+            self.assertTrue("payrollReport" in result.json)
+            self.assertTrue(len(result.json["payrollReport"]) == 1, result.json["payrollReport"])
+            self.assertTrue("employeeReports" in result.json["payrollReport"])
+            self.assertTrue(len(result.json["payrollReport"]["employeeReports"]) == 1, result.json["payrollReport"])
+            self.assertEqual(result.json["payrollReport"]["employeeReports"][0], mock_return)
