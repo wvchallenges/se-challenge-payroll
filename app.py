37fb8bb0b45ee4d1f51114b36775ebaf9efd4b8e
= <dharm1k987@gmail.com>
2021-03-18 16:03:16 -0400

    can now add each row from xls into dbs

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
-import db
+import re
 
+import db
+import csv
+import os
+
+def main():
+  con = db.initialize_db('example.db')
+  read_csv('time-report-42.csv', con)
+
+def read_csv(csv_file, con):
+  report_num = re.findall(r"time-report-(\d+)\.csv",csv_file)[0]
+  with open(csv_file) as f:
+    csv_reader = csv.DictReader(f)
+    line_count = 0
+    for row in csv_reader:
+      if line_count == 0:
+        print(f'Column names are {", ".join(row)}')
+      else:
+        db.insert_csv_row(con, row, report_num)
+      line_count += 1
 
 if __name__ == "__main__":
-  db.initialize_db('example.db')
+  os.remove('example.db')
+  main()
--- db.py
+++ db.py
@@ -1,5 +1,12 @@
 import sqlite3 as sql
+import datetime
+import time
 
+def exception_handler(con, e, msg):
+  if con:
+    con.rollback()
+
+  print(f"Unexpected error. {msg}: {e.args[0]}")
 
 def initialize_db(filename):
   con, cur = None, None
@@ -9,17 +16,19 @@ def initialize_db(filename):
     cur = con.cursor()
 
     # create tables
-    cur.execute('''CREATE TABLE IF NOT EXISTS EMPLOYEE (
+    cur.execute('''CREATE TABLE IF NOT EXISTS EMPLOYEES (
     id INTEGER PRIMARY KEY
     )''')
 
-    cur.execute('''CREATE TABLE IF NOT EXISTS JOB (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
+    cur.execute('''CREATE TABLE IF NOT EXISTS JOBS (
+    name TEXT PRIMARY KEY,
     rate INTEGER NOT NULL
     )''')
 
-    cur.execute('''CREATE TABLE IF NOT EXISTS EMPLOYEE_LOG (
+    cur.execute('''INSERT INTO JOBS (name, rate) VALUES (?,?)''', ('A', 20,))
+    cur.execute('''INSERT INTO JOBS (name, rate) VALUES (?,?)''', ('B', 30,))
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS EMPLOYEE_LOGS (
     employee_id INTEGER NOT NULL,
     log_date DATE NOT NULL,
     hours INTEGER NOT NULL,
@@ -32,12 +41,62 @@ def initialize_db(filename):
     )''')
 
     con.commit()
+    cur.close()
+    return con
   except Exception as e:
-    if con:
-      con.rollback()
-
-    print(f"Unexpected error. Cannot continue: {e.args[0]}")
+    exception_handler(con, e, "Cannot continue")
     exit(1)
-  finally:
-    if con:
-      con.close()
+
+def insert_employee_check(con, emp_id):
+  cur = con.cursor()
+  cur.execute('''INSERT OR REPLACE INTO EMPLOYEES (id) VALUES (?)''', (emp_id,))
+  cur.close()
+
+def insert_job_check(con, job):
+  cur = con.cursor()
+  cur.execute('''SELECT * FROM JOBS WHERE name=?''', (job,))
+
+  rows = cur.fetchall()
+  cur.close()
+  if not rows:
+    return False
+
+  return True
+
+def insert_csv_row(con, row, report_num):
+  date, hours, emp_id, job = row['date'], row['hours worked'], \
+                             row['employee id'], row['job group']
+
+  # make sure employee exists in the EMPLOYEE table, if not, insert
+  insert_employee_check(con, emp_id)
+
+  # make sure job exists in the JOB table, it should because we pre populate
+  if not insert_job_check(con, job):
+    print(f"Will not insert row because job {job} does not exist in JOB table")
+    return
+
+  # convert dd/mm/yyyy into datetime
+  try:
+    log_date = time.mktime(datetime.datetime.strptime(date, '%d/%m/%Y').timetuple())
+  except Exception as e:
+    # time.mktime(dt.timetuple())
+    print(f"Unable to convert date {date} to datetime: {e}")
+    return
+
+  # insert into EMPLOYEE_LOG
+  cur = con.cursor()
+  try:
+    cur.execute('''INSERT INTO EMPLOYEE_LOGS (employee_id, log_date, hours, job_name, report_num) VALUES (?,?,?,?,?)''',
+                (emp_id, log_date, hours, job, report_num))
+    con.commit()
+    print('Inserted row')
+    cur.close()
+    return con
+  except Exception as e:
+    exception_handler(con, e, "Skipping row")
+
+
+
+
+
+
